8744e9453f487ca95441fd0bf2e59f6f16cf5934
soakulo <125950641+soakulo@users.noreply.github.com>
2023-09-23 12:41:11 +0300

    Add files via upload

---
+++ ConsoleApp6/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int number;
+            Console.WriteLine("Введите число");
+            number = Convert.ToInt32(Console.ReadLine());
+            if (number < 7)
+            {
+                Console.WriteLine("Yes");
+            }
+            else if (number > 10)
+            {
+                Console.WriteLine("No");
+            }
+            else if (number ==9)
+            {
+                Console.WriteLine("Error");
+            }
+        }
+    }
+}
