2cb2105ac7e8182229b7b0eb0b3ee0489830dfb1
soakulo <125950641+soakulo@users.noreply.github.com>
2023-09-23 12:13:28 +0300

    Add files via upload

---
+++ CSLight/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSLight
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberOne;
+            int numberTwo;
+            int result;
+            Console.WriteLine("Ведите число:");
+            numberOne = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Ведите второе число:");
+            numberTwo = Convert.ToInt32(Console.ReadLine());
+            result = numberOne + numberTwo;
+            Console.WriteLine($"Результат: {result}");
+        }
+    }
+}
---
+++ ConsoleApp4/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int numb1, numb2, numb3, result;
+            Console.WriteLine("Введите первое число");
+            numb1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите второе число");
+            numb2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите третье число");
+            numb3 = Convert.ToInt32(Console.ReadLine());
+            if (numb1 > numb2)
+            {
+                result = numb1;
+                if (result < numb3)
+                {
+                    result = numb3;
+                    Console.WriteLine(result);
+                }
+                else
+                {
+                    Console.WriteLine(result);
+                }
+            }
+            else if (numb1 < numb2)
+            {
+                result = numb2;
+                if (result <numb3)
+                {
+                    result = numb3;
+                    Console.WriteLine(result);
+                }
+                else
+                {
+                    Console.WriteLine(result);
+                }
+
+            }
+
+        }
+    }
+}
